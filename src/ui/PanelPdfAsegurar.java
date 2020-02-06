e5572391b7930695f411a1e2478010263fe69a24
acernacho <ignacio.a.arg@gmail.com>
2020-02-05 23:15:55 -0300

    Changed to gridLayout, added textArea and checkbox

--- src/ui/PanelPdfAsegurar.java
+++ src/ui/PanelPdfAsegurar.java
@@ -1,12 +1,12 @@
 package ui;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-
-
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class PanelPdfAsegurar extends JPanel {
 
@@ -16,11 +16,18 @@ public class PanelPdfAsegurar extends JPanel {
     private static final long serialVersionUID = 1L;
     private JLabel headerLabel = new JLabel("Seleccionar PDF a aplanar");
     private JButton btnAsegurar = new JButton("Asegurar Pdf");
+    private JCheckBox checkBox = new JCheckBox("Encriptar");
+    private JTextField textArea = new JTextField("");
     
     public PanelPdfAsegurar() {
-        setLayout(new FlowLayout(FlowLayout.CENTER, 50,50));
+        setLayout(new GridLayout(3,3, 5 ,5));
+        
+        textArea.setEditable(false);
         add(headerLabel);
         add(btnAsegurar);
+        add(checkBox);
+        add(textArea);
+        
     }
 
     public JButton getBtnAsegurar() {
@@ -30,5 +37,14 @@ public class PanelPdfAsegurar extends JPanel {
     public JLabel getHeaderLabel() {
         return headerLabel;
     }
+
+    public JCheckBox getCheckBox() {
+        return checkBox;
+    }
+
+    public JTextField getTextArea() {
+        return textArea;
+    }
+
     
 }
